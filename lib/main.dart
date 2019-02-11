ab0e5a954b64ea1e1c511ab553aad714825a5c16
Prof-MohamedAtef <proofesser@gmail.com>
2019-02-11 06:59:18 -0800

    Changed Theme Color to Red

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       title: 'Startup Name Generator',
       home: new RandomWords(),
       theme: new ThemeData(
-        primaryColor: Colors.white
+        primaryColor: Colors.red
       ),
     );
   }
